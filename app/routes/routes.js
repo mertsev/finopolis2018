ba9a73211f235d1c949609b83f098d650c9467c6
mertsev <mse1996@gmail.com>
2018-08-28 22:37:33 +0700

    new api methods

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,8 @@
 module.exports = function(app, db) {
   
   var ObjectID = require('mongodb').ObjectID;
+
+  // Get agent info by ID
     app.get('/agents/:id', (req, res) => {
       const id = req.params.id;
       const details = { '_id': new ObjectID(id) };
@@ -13,8 +15,15 @@ module.exports = function(app, db) {
       });
     });
 
+  // Create agent
   app.post('/agents', (req, res) => {
-      const agent = { text: req.body.body, title: req.body.title };
+      const agent = { 
+        name: req.body.name, 
+        description: req.body.description,
+        rating: 0,
+        orfrating: parseInt(req.body.orf, 10),
+        sparkrating: parseInt(req.body.spark, 10)
+      };
       db.collection('agents').insert(agent, (err, result) => {
         if (err) { 
           res.send({ 'error': 'An error has occurred' }); 
@@ -24,6 +33,39 @@ module.exports = function(app, db) {
       });
     });
 
+  // Create review
+  app.post('/review', (req, res) => {
+      const review = { 
+        reviewer: req.body.reviewer,
+        agent_id: req.body.agent_id, 
+        weight: parseInt(req.body.weight, 10),
+        rating: parseInt(req.body.rating, 10),
+        message: req.body.message
+      };
+      db.collection('reviews').insert(review, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+      }
+    });
+  });
+
+  // Get review info by ID
+  app.get('/reviews/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('reviews').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+
+  
+  // Delete agent by ID
   app.delete('/agents/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
@@ -36,6 +78,7 @@ module.exports = function(app, db) {
     });
   });
 
+  // Change agent info by ID
   app.put ('/agents/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
@@ -49,6 +92,7 @@ module.exports = function(app, db) {
     });
   });
 
+  // Get all agents
   app.get('/agents/', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
