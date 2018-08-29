534a3febb0810561e1b0a702a9ddc3b973d1f6ab
Антон Удод <udod@aigeo.ru>
2018-08-29 20:33:00 +0700

    Some fronty front

--- client/src/components/pages/AgentsList.vue
+++ client/src/components/pages/AgentsList.vue
@@ -6,6 +6,22 @@
         <h3 class="col-lg-12 col-xs-12"> Список контрагентов </h3>
         section.panel.panel-success( v-if="posts.length" )
           .panel-heading
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">Фильтр</span>
+            </div>
+            <input type="text" class="form-control " v-model="fName" placeholder="Название компании" aria-label="Название компании" aria-describedby="basic-addon1">
+            <input class="form-control col-lg-1" type="number" v-model="fRate" placeholder="Рейтинг" id="replyNumber" min="0" max="5"step="1" data-bind="value:replyNumber" />
+            <select class="custom-select col-lg-3" v-model="fCate" aria-label="Example select with button addon">
+              <option selected>Категория</option>
+              <option value="Строительные компании">Строительные компании</option>
+              <option value="Питание">Питание</option>
+              <option value="Консалтинговые услуги">Консалтинговые услуги</option>
+            </select>
+            <div class="input-group-append ">
+              <button v-on:click="sendFilter()" class="btn btn-outline-secondary" type="button">Применить</button>
+            </div>
+          </div>
           <table class="table table-striped">
             tr
               th Название 
@@ -37,7 +53,10 @@
     name: 'AgentsListPage',
     data () {
       return {
-        posts: []
+        posts: [],
+        fName: '',
+        fCate: '',
+        fRate: 0
       }
     },
     methods: {
@@ -45,6 +64,12 @@
         const response = await AgentsService.getAgents()
         console.log(response.data)
         this.posts = response.data
+      },
+      async sendFilter () {
+        
+        const response = await AgentsService.getFilteredAgents(this.fName, this.fCate, this.fRate)
+        console.log(response)
+        /* this.posts = response.data */
       }
     },
     mounted () {
--- client/src/services/AgentsService.js
+++ client/src/services/AgentsService.js
@@ -1,7 +1,26 @@
 import api from '@/services/api'
+import querystring from 'querystring';
+
 
 export default {
   getAgents () {
+    
     return api().get('agents')
+  },
+  getFilteredAgents(fName, fCate, fRate) {
+    alert( ''+fName + fCate + fRate)
+    return api().post('agents', querystring.stringify({  name: fName,
+        description: fCate,
+        orf: fRate,
+        spark: fRate 
+                    
+            }), {
+            headers: { 
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+            }).then(function(response) {
+                console.log(response);
+            }
+    )
   }
 }
